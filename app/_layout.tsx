cba3403f508fdad8e9f1386c8b8b820ffcc024f0
gabrielTor <gabriel_torres2012@yahoo.com>
2024-10-21 14:03:22 -0300

    remove ( layout cleanup )

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,11 @@
 import { ThemeProvider } from "@/context/Theme.context";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
-
-SplashScreen.preventAutoHideAsync();
 
 const options = {
   headerShown: false,
 };
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) SplashScreen.hideAsync();
-  }, [loaded]);
-
-  if (!loaded) return null;
-
   return (
     <ThemeProvider>
       <Stack>
